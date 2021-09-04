e4f6dc513ba96b528f67f77d399696fee4c26d37
bigdata <bigdata@qq.com>
2021-09-04 10:06:25 +0800

    测试代码第二次修改

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
-Class HelloWorld{
-  public static void main(String args){
-    System.out.println("Hello World!");
-  }  
+Class HelloWorld {
+
+	public static void main(String args){
+		System.out.println("Hello World!");
+		System.out.println("测试添加一行代码！");
+	}
+	
 }
