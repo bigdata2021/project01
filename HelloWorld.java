10bef73341517a88f4a5916365fbdb695068176e
bigdata <bigdata@qq.com>
2021-09-04 17:10:10 +0800

    coder-lisi用户，第一次修改代码

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,6 +6,7 @@ Class HelloWorld {
 		System.out.println("测试添加第二行代码！");
 		System.out.println("测试添加一行代码！");
 	        System.out.println("bigdata01子分支，添加添加一行代码！");
+                System.out.println("coder-lisi用户，添加一行代码！");
 	}
 	
 }
